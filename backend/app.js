11d4db87093e59130032171ed0918d65aaab4e45
Veeti Kokkonen <vetsa.kokkonen@gmail.com>
2026-03-17 11:50:46 +0200

    autentikointi toimii

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,11 @@ const PORT = 3000;
 const app = express();
 const studentRouter = require('./routes/student');
 const loginRouter = require('./routes/login');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
 
 app.use(express.json());
+dotenv.config;
 
 app.get('/',function(request,response){
 response.send('Testi Pelaa!!');
@@ -12,6 +15,7 @@ response.send('Testi Pelaa!!');
 
 
 app.use('/login',loginRouter);
+app.use(authenticateToken);
 app.use('/student',studentRouter);
 
 
@@ -19,5 +23,27 @@ app.use('/student',studentRouter);
 app.listen(PORT,function(){
     console.log('Portti '+PORT);
 });
+
+function authenticateToken(request, response, next) {
+    const authHeader = request.headers['authorization'];
+
+    if (!authHeader) {
+      return response.sendStatus(401);
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    if (!token) {
+      return response.sendStatus(401);
+    }
+
+    jwt.verify(token, process.env.MY_SECRET, function(err, user) {
+      if (err) {
+        return response.sendStatus(403);
+      }
+      request.user = user;
+      next();
+    })
+  }
 
 module.exports = app;
--- backend/routes/student.js
+++ backend/routes/student.js
@@ -3,6 +3,7 @@ const student = require('../models/student_model');
 const router = express.Router();
 
 router.get('/',function(request,response){
+    console.log(request.user);
     student.getAll(function(err,result){
         if(err){
             response.send(err);
@@ -25,7 +26,10 @@ router.get('/:uname',function(request,response){
 });
 
     router.post('/',function(request,response){
-        console.log(request.body);
+        console.log(request.user);
+        if(request.user.role != 'admin' && request.user.role != 'teacher'){
+            return response.status(401).json({"error":"sinulla ei ole oikeutta tähän resurssiin"});
+        }
     student.add(request.body, function(err, result){
         if(err){
             response.send(err);
